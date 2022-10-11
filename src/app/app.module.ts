340d8041aa28fd36e97bfc1cfc4c07ea816b0f38
JackOliveira1 <jackoliveira14adm10@gmail.com>
2022-10-11 15:58:02 -0300

    Rotas configuradas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import {    NgModule              } from '@angular/core';
+import {    BrowserModule         } from '@angular/platform-browser';
+import {    FormsModule           } from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 
-import { DropdownDirective } from './shared/dropdown.directive';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
+import {    AppComponent          } from './app.component';
+import {    HeaderComponent       } from './header/header.component';
+import {    RecipesComponent      } from './recipes/recipes.component';
+import {    RecipeListComponent   } from './recipes/recipe-list/recipe-list.component';
+import {    RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import {    ShoppingListComponent } from './shopping-list/shopping-list.component';
+import {    ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import {    RecipeItemComponent   } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import {    DropdownDirective     } from './shared/dropdown.directive';
+import {    ShoppingListService   } from './shopping-list/shopping-list.service';
+import {    AppRoutingModule      } from './app.routing.module';
+import {    RecipeStarComponent   } from './recipes/recipe-start/recipe-star.component';
+import {    RecipeEditComponent   } from './recipes/recipe-edit/recipe-edit.component';
+
 
 @NgModule({
   declarations: [
@@ -24,11 +28,14 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
     ShoppingListComponent,
     ShoppingEditComponent,
     RecipeItemComponent,
-    DropdownDirective
+    DropdownDirective,
+    RecipeStarComponent,
+    RecipeEditComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [ShoppingListService],
   bootstrap: [AppComponent]
--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Recipe } from '../recipe.model';
-import { RecipeService } from '../recipe.service';
+import { Component, OnInit      } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Recipe                 } from '../recipe.model';
+import { RecipeService          } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -8,12 +9,19 @@ import { RecipeService } from '../recipe.service';
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
+  recipe: Recipe;
+  id: number;
 
-  @Input() recipe: Recipe;
-
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute) { }
 
   ngOnInit(){
+    this.route.params
+    .subscribe(
+      (params: Params) => {
+        this.id = +params['id'];
+        this.recipe = this.recipeService.getRecipe(this.id);
+      }
+    )
   }
   
   onAddToShoppingList(){
